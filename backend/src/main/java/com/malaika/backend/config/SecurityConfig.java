afa2e7a2f7f5fe82151ce592c07f09c08c9e4141
MalaikaSajid <malaikasajid05@gmail.com>
2026-06-07 13:49:24 +0500

    Improved Security Configuration after Sonar Qube

--- backend/src/main/java/com/malaika/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/malaika/backend/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -28,7 +29,7 @@ public class SecurityConfig {
             throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
 
                 .cors(cors -> cors.configurationSource(
                         corsConfigurationSource()))
